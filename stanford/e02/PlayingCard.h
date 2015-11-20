fb3a8811343dc5a0092ca8909e5cc2cb6d251e4e
Feng <haoran.feng@sap.com>
2015-11-19 17:00:03 -0800

    pass compile

--- stanford/e02/PlayingCard.h
+++ stanford/e02/PlayingCard.h
@@ -4,4 +4,8 @@
 
 @property (strong, nonatomic) NSString *suit;
 @property (nonatomic) NSUInteger rank;
+
++ (NSArray *)validSuits;
++ (NSUInteger)maxRank;
+
 @end
--- stanford/e02/PlayingCard.m
+++ stanford/e02/PlayingCard.m
@@ -4,6 +4,48 @@
 
 - (NSString *) contents
 {
-    return [NSString stringWithFormat:@"%d%@", self.rank, self.suit];
+    NSArray *rankStrings = [PlayingCard rankStrings];
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
+
+// plus means static method
++ (NSArray *) validSuits
+{
+    return @[@"",@"",@"",@""];
+}
+
++ (NSArray *)rankStrings
+{
+    return @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9",@"10",@"J",@"Q", @"K"];
+}
+
++ (NSUInteger)maxRank
+{
+    return [[self rankStrings] count]-1;
+}
+
+
+
+// we provide both setter and getter, so the @synthesize is needed
+@synthesize suit = _suit;
+
+- (void)setSuit:(NSString *)suit
+{
+    if ([ [PlayingCard validSuits]containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+- (NSString *)suit
+{
+    return _suit ? _suit : @"?";
+}
+
+- (void)setRank:(NSUInteger) rank
+{
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
+
 @end
